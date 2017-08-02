e4add56ebe6c9133bdf8b6a8cb1c7d544573066a
Andrey Barchuk <ndreybar@gmail.com>
2017-08-02 11:40:34 +0300

    added mongodb connect

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,9 +1,14 @@
 function routeNotes(app, db) {
   app.post('/notes', function (req, res) {
-
-    console.log(req.body)
-    res.send('this is a notes!');
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   });
-}
+};
 
 module.exports = routeNotes;
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 const app = express();
 
 const routes = require('./app/routes');
-
 const port = process.env.PORT || 5000;
 
-routes(app, {});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, function () {
-  console.log('Example app listening on port');
-});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  routes(app, database);
+  app.listen(port, function () {
+    console.log('Example app listening on port');
+  });
+})
